c549e613160db3b2f3fb5850a1d842434668bb04
creflobrian@gmail.com <creflobrian@gmail.com>
2024-04-03 21:01:56 +0300

    complete array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+  return drivers.filter(drivers => drivers.toLowerCase().includes(string.toLowerCase()))
+}
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(drivers => drivers.toLowerCase().startsWith(string.toLowerCase()))
+}
+function matchName(drivers,string){
+    const result = drivers.filter(word => word.name === string)
+    return result
+  }
